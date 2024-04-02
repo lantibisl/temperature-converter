657bb630825f15d114cbe38804203873ac0eb7d2
Lantib Islami <lantibislami1@gmail.com>
2024-04-02 16:22:09 +0700

    Membuat layar dapat discroll pada layar kecil

--- app/src/main/java/org/d3if0158/temperatureconverter/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0158/temperatureconverter/screen/MainScreen.kt
@@ -9,13 +9,18 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -38,7 +43,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if0158.temperatureconverter.R
@@ -48,12 +52,13 @@ import org.d3if0158.temperatureconverter.ui.theme.TemperatureConverterTheme
 @Composable
 fun MainScreen() {
     Scaffold(topBar = {
-        TopAppBar(title = {
-            Text(text = stringResource(id = R.string.app_name))
-        }, colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary
-        )
+        TopAppBar(
+            title = {
+                Text(text = stringResource(id = R.string.app_name))
+            }, colors = TopAppBarDefaults.mediumTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary
+            )
         )
     }) { padding ->
         ScreenContent(Modifier.padding(padding))
@@ -69,6 +74,10 @@ fun ScreenContent(modifier: Modifier) {
         stringResource(id = R.string.reaumur),
         stringResource(id = R.string.kelvin),
         stringResource(id = R.string.fahrenheit)
+    )
+
+    val otherUnitOptions = listOf(
+        "° C", "° R", "K", "° F"
     )
 
     val isExpanded1 = rememberSaveable {
@@ -95,29 +104,77 @@ fun ScreenContent(modifier: Modifier) {
         mutableFloatStateOf(0f)
     }
 
+    var unitError1 by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    var unitError2 by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    var valueError by rememberSaveable {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalAlignment =Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        UnitSelector(isExpanded1 = isExpanded1, temperatureUnit1 = temperatureUnit1, temperatureUnit2 = temperatureUnit2, unitOptions = unitOptions)
-        UnitSelector(isExpanded1 = isExpanded2, temperatureUnit1 = temperatureUnit2, temperatureUnit2 = temperatureUnit1, unitOptions = unitOptions)
+        UnitSelector(
+            isExpanded1 = isExpanded1,
+            temperatureUnit1 = temperatureUnit1,
+            temperatureUnit2 = temperatureUnit2,
+            unitOptions = unitOptions,
+            unitError1
+        )
+        UnitSelector(
+            isExpanded1 = isExpanded2,
+            temperatureUnit1 = temperatureUnit2,
+            temperatureUnit2 = temperatureUnit1,
+            unitOptions = unitOptions,
+            unitError2
+        )
         OutlinedTextField(
             value = initialValue,
             onValueChange = { initialValue = it },
             label = { Text(text = stringResource(id = R.string.initial_value)) },
+            isError = valueError,
+            trailingIcon = {
+                IconPicker(
+                    isError = valueError,
+                    when (temperatureUnit1.value) {
+                        unitOptions[0] -> otherUnitOptions[0]
+                        unitOptions[1] -> otherUnitOptions[1]
+                        unitOptions[2] -> otherUnitOptions[2]
+                        unitOptions[3] -> otherUnitOptions[3]
+                        else -> ""
+                    }
+                    )
+                           },
+            supportingText = { ErrorHint(isError = valueError) },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number, imeAction = ImeAction.Done
             ),
             modifier = Modifier.fillMaxWidth()
         )
-        Button(onClick = {
-            finalValue = temperatureConverter(context, temperatureUnit1.value, temperatureUnit2.value, initialValue.toFloat())
-
-        },
+        Button(
+            onClick = {
+                unitError1 = temperatureUnit1.value == ""
+                unitError2 = temperatureUnit2.value == ""
+                valueError = initialValue == ""
+                if (unitError1 || unitError2 || valueError) return@Button
+                finalValue = temperatureConverter(
+                    context,
+                    temperatureUnit1.value,
+                    temperatureUnit2.value,
+                    initialValue.toFloat()
+                )
+            },
             modifier = Modifier.padding(top = 8.dp),
             contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
         ) {
@@ -128,26 +185,44 @@ fun ScreenContent(modifier: Modifier) {
                 modifier = Modifier.padding(vertical = 8.dp),
                 thickness = 1.dp
             )
-            Text(text = stringResource(id = R.string.final_value_x, initialValue.toFloat(), temperatureUnit1.value.lowercase(), temperatureUnit2.value.lowercase(), finalValue),
+            Text(
+                text = stringResource(
+                    id = R.string.final_value_x,
+                    initialValue.toFloat(),
+                    temperatureUnit1.value.lowercase(),
+                    temperatureUnit2.value.lowercase(),
+                    finalValue
+                ),
                 style = MaterialTheme.typography.headlineLarge,
                 textAlign = TextAlign.Center
-                )
+            )
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UnitSelector(isExpanded1: MutableState<Boolean>, temperatureUnit1: MutableState<String>, temperatureUnit2: MutableState<String>, unitOptions: List<String>) {
-    Box{
-        ExposedDropdownMenuBox(expanded = isExpanded1.value, onExpandedChange = { isExpanded1.value = it }) {
+fun UnitSelector(
+    isExpanded1: MutableState<Boolean>,
+    temperatureUnit1: MutableState<String>,
+    temperatureUnit2: MutableState<String>,
+    unitOptions: List<String>,
+    isError: Boolean
+) {
+    Box {
+        ExposedDropdownMenuBox(
+            expanded = isExpanded1.value,
+            onExpandedChange = { isExpanded1.value = it }) {
             OutlinedTextField(
                 value = temperatureUnit1.value,
                 onValueChange = {},
-                label = { Text(text = stringResource(id = R.string.initial_unit))},
+                label = { Text(text = stringResource(id = R.string.initial_unit)) },
+                isError = isError,
+                supportingText = { ErrorHint(isError = isError)},
                 readOnly = true,
                 trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded1.value)
+                    if (isError) Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+                    else ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded1.value)
                 },
                 modifier = Modifier
                     .menuAnchor()
@@ -167,9 +242,12 @@ fun UnitSelector(isExpanded1: MutableState<Boolean>, temperatureUnit1: MutableSt
                 )
             )
 
-            ExposedDropdownMenu(expanded = isExpanded1.value, onDismissRequest = { isExpanded1.value = false }) {
+            ExposedDropdownMenu(
+                expanded = isExpanded1.value,
+                onDismissRequest = { isExpanded1.value = false }) {
                 val temperatureUnitIndex2 = unitOptions.indexOf(temperatureUnit2.value)
-                val filteredUnitOptions2 = unitOptions.filterIndexed { index, _ -> index != temperatureUnitIndex2 }
+                val filteredUnitOptions2 =
+                    unitOptions.filterIndexed { index, _ -> index != temperatureUnitIndex2 }
 
                 when (temperatureUnitIndex2) {
                     in unitOptions.indices -> {
@@ -183,6 +261,7 @@ fun UnitSelector(isExpanded1: MutableState<Boolean>, temperatureUnit1: MutableSt
                             )
                         }
                     }
+
                     else -> {
                         unitOptions.forEach { text ->
                             DropdownMenuItem(
@@ -200,31 +279,54 @@ fun UnitSelector(isExpanded1: MutableState<Boolean>, temperatureUnit1: MutableSt
     }
 }
 
+@Composable
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+        Text(text = unit)
+    }
+}
 
-fun temperatureConverter(context: Context, initialUnit: String, finalUnit: String, initialValue: Float): Float {
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(id = R.string.invalid_input))
+    }
+}
+
+fun temperatureConverter(
+    context: Context,
+    initialUnit: String,
+    finalUnit: String,
+    initialValue: Float
+): Float {
     var finalValue = 0f
-    val unitOptions = listOf<String>(
+    val unitOptions = listOf(
         context.resources.getString(R.string.celsius),
         context.resources.getString(R.string.reaumur),
         context.resources.getString(R.string.kelvin),
         context.resources.getString(R.string.fahrenheit)
     )
-    when (initialUnit){
+    when (initialUnit) {
         unitOptions[0] -> when (finalUnit) {
             unitOptions[1] -> finalValue = initialValue / 5 * 4
             unitOptions[2] -> finalValue = initialValue + 273
             unitOptions[3] -> finalValue = initialValue / 5 * 9 + 32
         }
+
         unitOptions[1] -> when (finalUnit) {
             unitOptions[0] -> finalValue = initialValue / 4 * 5
             unitOptions[2] -> finalValue = initialValue / 4 * 5 + 273
             unitOptions[3] -> finalValue = initialValue / 4 * 9 + 32
         }
-        unitOptions[2]  -> when (finalUnit) {
+
+        unitOptions[2] -> when (finalUnit) {
             unitOptions[0] -> finalValue = initialValue - 273
             unitOptions[1] -> finalValue = (initialValue - 273) / 5 * 4
-            unitOptions[3] -> finalValue = (initialValue -273) / 5 * 9 + 32
+            unitOptions[3] -> finalValue = (initialValue - 273) / 5 * 9 + 32
         }
+
         unitOptions[3] -> when (finalUnit) {
             unitOptions[0] -> finalValue = (initialValue - 32) / 9 * 5
             unitOptions[1] -> finalValue = (initialValue - 32) / 9 * 4
